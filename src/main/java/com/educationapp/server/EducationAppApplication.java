e1b0745e62fde0faae760519ea9e38ce6a4fa010
HappyMary16 <m.borodin.1999@gmail.com>
2019-08-26 15:52:16 +0300

    Initial commit

---
+++ src/main/java/com/educationapp/server/EducationAppApplication.java
@@ -0,0 +1,13 @@
+package com.educationapp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/educationapp/server/models/UserType.java
@@ -0,0 +1,11 @@
+package com.educationapp.server.models;
+
+public enum UserType {
+    ADMIN,
+    UNIVERSITY_ADMIN,
+    SCHOOL_ADMIN,
+    STUDENT,
+    PUPIL,
+    PARENT,
+    USER
+}
