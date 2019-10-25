42fc945cc2f730aaa58d4ef4cf5ea6b24539135d
HappyMary16 <m.borodin.1999@gmail.com>
2019-10-25 22:52:13 +0300

    change nick to nickname in create users script

--- src/main/resources/changeset/000_create_users.sql
+++ src/main/resources/changeset/000_create_users.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
 	id INT(11) NOT NULL AUTO_INCREMENT,
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
-	nick VARCHAR(50),
+	nickname VARCHAR(50),
 	password VARCHAR(50) NOT NULL,
 	phone VARCHAR(50),
 	email VARCHAR(50),
