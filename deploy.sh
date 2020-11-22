3207888326bde744abb1dfbcb3566143b047e09d
mborodin <m.borodin.1999@gmail.com>
2020-11-22 15:57:02 +0200

    added files for deploy to prod

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+rsync -crlOt ./target/EducationApp.jar ubuntu@ec2-18-189-141-10.us-east-2.compute.amazonaws.com:/home/ubuntu/educationapp/EducationApp.jar
+ssh ubuntu@ec2-18-189-141-10.us-east-2.compute.amazonaws.com sudo service educationapp restart
