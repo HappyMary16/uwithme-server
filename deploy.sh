0f0d70f4bfeeaad0dc01c2ed764cb0e2222117f6
mariia.borodin <m.borodin.1999@gmail.com>
2021-05-22 17:46:18 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-rsync -crlOt ./university-management-service/target/EducationApp.jar ubuntu@ec2-18-189-141-10.us-east-2.compute.amazonaws.com:/home/ubuntu/educationapp/EducationApp.jar
+rsync -crlOt ./university-with-me-service/target/EducationApp.jar ubuntu@ec2-18-189-141-10.us-east-2.compute.amazonaws.com:/home/ubuntu/educationapp/EducationApp.jar
 ssh ubuntu@ec2-18-189-141-10.us-east-2.compute.amazonaws.com sudo service educationapp restart
