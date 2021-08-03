9ce484798b5be9300c99a6f59afdde70a5355ff3
mariia.borodin <m.borodin.1999@gmail.com>
2021-08-04 00:39:50 +0300

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
-scp -r ./university-with-me-service/target/EducationApp.jar ubuntu@ec2-18-189-141-10.us-east-2.compute.amazonaws.com:/home/ubuntu/educationapp/EducationApp.jar
-ssh ubuntu@ec2-18-189-141-10.us-east-2.compute.amazonaws.com sudo service educationapp restart
+cd ./university-with-me-service/target
+ls -la
+
