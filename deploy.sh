ff2c262958b74e66c90e4de781c904907459a597
Mikhail Groshevoy <developer.misha999777@gmail.com>
2021-08-24 00:40:31 +0300

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-scp ./EducationApp/EducationApp.jar ubuntu@ec2-18-189-141-10.us-east-2.compute.amazonaws.com:/home/ubuntu/educationapp/EducationApp.jar
-ssh ubuntu@ec2-18-189-141-10.us-east-2.compute.amazonaws.com sudo service educationapp restart
+scp ./EducationApp/EducationApp.jar root@109.87.198.211:/home/misha/educationapp/EducationApp.jar
+ssh root@109.87.198.211 service educationapp restart
