c3a6150295c1b95da91d3ec62bbd9d4dde6658cb
mborodin <m.borodin.1999@gmail.com>
2023-03-19 16:23:45 +0100

    fixed tests sut up

--- uwithme-func-tests/src/test/java/com/mborodin/uwm/AbstractBaseTest.java
+++ uwithme-func-tests/src/test/java/com/mborodin/uwm/AbstractBaseTest.java
@@ -1,33 +1,23 @@
 package com.mborodin.uwm;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import java.util.Objects;
 
 import com.mborodin.uwm.api.RegisterApi;
 import com.mborodin.uwm.api.enums.Role;
-import com.mborodin.uwm.api.structure.GroupApi;
 import com.mborodin.uwm.client.client.UwmClient;
 import com.mborodin.uwm.client.client.core.TenantClient;
-import com.mborodin.uwm.client.config.AuthClientConfiguration;
-import com.mborodin.uwm.config.KeycloakConfig;
-import com.mborodin.uwm.tests.GroupTests;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
-import org.springframework.test.context.event.annotation.BeforeTestExecution;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@Slf4j
 @SpringBootTest
 @ActiveProfiles("test")
 @EnableConfigurationProperties
@@ -47,7 +37,10 @@ public class AbstractBaseTest {
     @BeforeEach
     public void setupUsers() {
         if (!isTenantCreated) {
-            isTenantCreated = Objects.nonNull(tenantClient.getUniversity());
+            try {
+                isTenantCreated = Objects.nonNull(tenantClient.getUniversity());
+            } catch (final Throwable ignored) {
+            }
         }
 
         if (!isTenantCreated) {
