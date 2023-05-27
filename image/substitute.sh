e666a3c0c34d4492b97ed4d84bef9d6616b0e7ac
mborodin <m.borodin.1999@gmail.com>
2023-05-27 16:00:01 +0200

    updated Dockerfile

---
+++ image/substitute.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCHEMA=http://
+
+if [ "$SSL_ENABLED" = "true" ]; then
+    SCHEMA=https://
+fi
+
+export SCHEMA="${SCHEMA}"
+
+envsubst < application.properties.tmpl > application.properties
